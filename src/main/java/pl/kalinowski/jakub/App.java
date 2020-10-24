5067016d14107e2be09ef0c6a56b5b91913c59fd
Arantog <arantog@gmail.com>
2020-10-24 10:10:36 +0200

    Initial commit

---
+++ src/main/java/pl/kalinowski/jakub/App.java
@@ -0,0 +1,13 @@
+package pl.kalinowski.jakub;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
