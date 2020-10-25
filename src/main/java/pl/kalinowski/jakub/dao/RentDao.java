7440c9367c229e67f87f96632fbeeb2a3b385c34
Arantog <arantog@gmail.com>
2020-10-25 13:03:15 +0100

    RentDao

---
+++ src/main/java/pl/kalinowski/jakub/dao/RentDao.java
@@ -0,0 +1,9 @@
+package pl.kalinowski.jakub.dao;
+
+import pl.kalinowski.jakub.model.Rent;
+
+public class RentDao extends AbstractDao<Rent> {
+    public RentDao() {
+        super(Rent.class);
+    }
+}
